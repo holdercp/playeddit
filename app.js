fe290f49006284118d8737bfe2428d440aadf0aa
holdercp <chrispholder@gmail.com>
2019-01-18 19:39:57 -0500

    Get passport installed and running

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: '518406354a4c4a939234351554badffc',
+      clientSecret: '60dec744d3974ea19b4ec8ba02b87c0f',
+      callbackURL: 'http://localhost:3000/auth/spotify/callback',
+    },
+    (accessToken, refreshToken, expires_in, profile, done) => done(null, profile),
+  ),
+);
+
+app.get('/auth/spotify', passport.authenticate('spotify'), (req, res) => {
+  // The request will be redirected to spotify for authentication, so this
+  // function will not be called.
+});
+
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', {
+    failureRedirect: '/login',
+    session: false,
+  }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  },
+);
 
 module.exports = app;
