18b178701111fc3a4b27998e56ca50eb8aaba162
holdercp <chrispholder@gmail.com>
2019-01-23 19:57:06 -0500

    Install body-parser

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const { passport } = require('./auth');
 
-const indexRouter = require('./routes/index');
 const playlistRouter = require('./resources/playlist').router;
 const subredditRouter = require('./resources/subreddit').router;
 const authRouter = require('./auth').router;
@@ -18,12 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(session({ secret: 'cats', resave: true, saveUninitialized: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/playlist', playlistRouter);
 app.use('/subreddit', subredditRouter);
