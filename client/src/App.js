5b671f4f14a2b9e48272a2631475d607ef27a12f
holdercp <chrispholder@gmail.com>
2019-02-02 15:15:07 -0500

    Add meterial UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <Typography component="h2" variant="h1" gutterBottom>
+          Hello World!
+        </Typography>
+      </React.Fragment>
     );
   }
 }
